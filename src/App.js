549b9bd7cc843fcff03852a83e161c6075a102f2
Lymonada <lymonada24@gmail.com>
2024-04-29 22:36:26 +0900

    Edit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function Header(props) {
   return <header>
-    <h1><a href="/">WEB</a></h1>
+    <h1><a href="/" onClick={function(event){
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 
 function Nav(props) {
+  const lis = []
+  for(let i = 0; i < props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
   return <nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {lis}
     </ol>
   </nav>
 }
 
 function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is boring.'},
+    {id:2, title:'css', body:'css is good.'},
+    {id:3, title:'js', body:'html is awesome.'}
+  ]
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="REACT" onChangeMode = {function(){
+        alert('Header')
+      }}></Header>
+      <Nav topics = {topics}></Nav>
+      <Article title ="Welcome" body="Hello, Web"></Article>
     </div>
   );
 }
